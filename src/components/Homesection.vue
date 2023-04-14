258063f6010fff2656093fea9e72928945d1d067
Brandon Tai <btai@bayviewglen.ca>
2023-04-14 09:47:55 -0400

    home page done

--- src/components/Homesection.vue
+++ src/components/Homesection.vue
@@ -1,12 +1,20 @@
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 <template>
     <div class="bg">
-    <div class="columns">
-        <div class="column">
-            <h1 id="home-header" style="font-weight:500" class="is-size-1">Where buyers meet sellers.</h1>
-        </div>
-    <div class="column">
-        
-    </div>
-</div>
+        <center>
+        <h1 id="home-header" style="" class="is-size-1">Find More For Less.</h1>
+        <div class="control">
+    <span><input class="input" type="text" placeholder="Search" ></span>
+    <span> <RouterLink to="/PostSearch">
+      <h1>
+      <FontAwesomeIcon icon="search"></FontAwesomeIcon>
+    </h1>
+    </RouterLink>
+  </span>
+
+  </div>
+    </center>
 </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 import 'bulma/css/bulma.css'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+library.add(faHatWizard)
+
 
 app.use(createPinia())
 app.use(router)
