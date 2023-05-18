0f5a85bdb899f2c2158f032a37daf6796de21615
Brandon Tai <btai@bayviewglen.ca>
2023-05-18 09:44:23 -0400

    searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,21 @@
 <template>
      <form class="example" action="">
         <div class="control">
-<input id="searchbar" class="input" type="text" placeholder="&#xF002;" >
+<input placeholder="&#xF002;" :style="{color: fontColor}" id="searchbar" class="input" type="text" >
   <RouterLink to="/PostSearch">
       <button class="button is-circle"><i class="fa-solid fa-arrow-right"></i></button>
     </RouterLink>
   </div>
 </form>
 </template>
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    fontColor: {
+      type: String,
+      default: 'black'
+    }
+  }
+}
+</script>
