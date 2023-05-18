4c13036545f129fbacbd87d601f94f25a70fd870
KaiyanChu <kchu@bayviewglen.ca>
2023-05-18 08:47:29 -0400

    chatbox messages

--- src/views/Chatbox.vue
+++ src/views/Chatbox.vue
@@ -1,12 +1,29 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
 import Messages from '../components/Messages.vue'
+import MessageContact from '../components/MessageContact.vue'
 
 </script>
 
 <template>
   <NavBar></NavBar>
-  <div class="containercenter"> 
-  <Messages></Messages>
+  <div>
   </div>
+  <div class="columns">
+    <div class="column is-one-third">
+      <MessageContact></MessageContact>
+      <MessageContact></MessageContact>
+      <MessageContact></MessageContact>
+      <MessageContact></MessageContact>
+      <MessageContact></MessageContact>
+      <MessageContact></MessageContact>
+
+    </div>
+    <div class="column">
+      <div class="containercenter"> 
+        <Messages></Messages>
+      </div>
+    </div>
+  </div>
+
   </template>
