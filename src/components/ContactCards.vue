35ea27c7e170eed4e9e30d18330fc09363e04794
KaiyanChu <kchu@bayviewglen.ca>
2023-06-06 14:13:09 -0400

    Co-authored-by: BrandonT <BrandonT310442@users.noreply.github.com>

---
+++ src/components/ContactCards.vue
@@ -0,0 +1,26 @@
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+let user = ref(null); // Initialize with null or any default value
+
+export default {
+  name: 'ContactCards',
+    props: {
+    username: {
+      type: String,
+      default: 'none'
+    }
+  },
+}
+
+</script>
+
+
+<template>
+    <div v-on="" class="card message-card" style="width: fit-content !important; height: fit-content !important;">
+        <div class="card-content">
+            <p class="username">{{username}}</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, accusantium.</p>
+        </div>
+    </div>
+</template>
--- src/components/MessageContact.vue
+++ src/components/MessageContact.vue
@@ -1,17 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import ContactCards from './ContactCards.vue'
+let user = ref(null); // Initialize with null or any default value
+let names = ref([]);
+let userid = ref([]);
+
+onMounted(() => {
+  axios.get("http://localhost:3000/currentUser", {
+    headers: {
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+    }
+  })
+    .then(response => {
+      user.value = response.data; // Store the fetched user data in the user ref
+      console.log(user.value.user.messages)
+      console.log(user.value.user.messages.length)
+      for (let i = 0; i < user.value.user.messages.length; i++){
+        console.log(user.value.user.messages[i].recipient.name);
+        names.value.push(user.value.user.messages[i].recipient.name);
+        userid.value.push(user.value.user.messages[i].recipient);
+}
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+</script>
+
 <template>
-<div class="card message-card" style="width: fit-content !important; height: fit-content !important;">
-    <div class="card-content">
-        <div class="columns">
-        <div class="column is-one-fifth">
-            <img src="../images/profile.png" alt="..." style="width:50px;"> 
-        </div>
-        <div class="column">
-            <p class="username">John Smith</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, accusantium.</p>
-        </div>
-    </div>
-    </div>
-</div>
+    <ContactCards v-for="name of names" :username="name"></ContactCards>
 </template>
 
 <style>
@@ -24,3 +43,7 @@
     }
 
 </style>
+
+<script >
+
+</script>
